e6dcf1d60e2bce78ee25a10c68c0e5c236e2d2b3
Eduardo Pérez <eduardojoseperezf@gmail.com>
2017-03-14 13:40:32 -0300

    TAC Lookup

--- redir.php
+++ redir.php
@@ -5,13 +5,20 @@ $Func = new CommonFunctions();
 session_start();
 $Func->CheckOperadora($_GET["Operadora"]);
 $Func->CheckTelefono($_GET["Telefono"]);
+
 if (ctype_digit($_GET["Telefono"])) {
-  $_SESSION["Alert"] = "Estás buscando un IMEI!";
-  header("Location: /");
-  die;
+  $TAC = mb_substr($_GET["Telefono"], 0, 8);
+  $query = "SELECT Slug from Telefono WHERE idTelefono = (SELECT idTelefono from TAC where TAC = '$TAC')";
+  $result = mysqli_query($conn, $query);
+  while ($row = mysqli_fetch_array($result)){
+    $TSlug = $row["Slug"];
+  }
 }
-$TInput = $Func->Slugify($_GET["Telefono"]);
-$Telefono = new Telefono(mysqli_real_escape_string($conn, $TInput));
-$TSlug = $Telefono->Slug;
+else {
+  $TInput = $Func->Slugify($_GET["Telefono"]);
+  $Telefono = new Telefono(mysqli_real_escape_string($conn, $TInput));
+  $TSlug = $Telefono->Slug;
+}
+
 $OSlug = $Func->Slugify($_GET["Operadora"]);
 header("Location: /ver/$TSlug/$OSlug");
